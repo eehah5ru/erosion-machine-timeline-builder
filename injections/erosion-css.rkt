03b427fcba4ad35f4d5374e5075cece247744ad0
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2020-07-09 16:58:15 +0300

    add weighted-random-element and css-font-faces

--- injections/erosion-css.rkt
+++ injections/erosion-css.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+
+(require math/base)
 (require racket/hash)
 
 (provide (all-defined-out))
@@ -20,6 +22,8 @@
                                      "shadow-cyan"))
 
 (define css-text-shadow-sizes (list "shadow-normal"))
+
+
 ;;;
 ;;; font size variants
 ;;;
@@ -27,6 +31,36 @@
                              "big"
                              "huge"
                              "large"))
+
+;;;
+;;; font-faces
+;;;
+(define css-font-faces (list '("font-default" . 75)
+                             '("font-desdemona" . 10)
+                             '("font-blackoak-std" . 10)
+                             '("font-bodoni-ornaments" . 5)))
+
+
+(define (weighted-random-element xs)
+  (define r (random-integer 0 (sum (map cdr xs))))
+
+
+  (define (f base ys)
+    (cond
+      [(empty? ys) (error "weighted-random-element: can't found value for " r)])
+
+    (define y (first ys))
+    (define y-weight (cdr y))
+    (define y-val (car y))
+
+    (cond
+      [(< r (+ base y-weight)) y-val]
+
+      [else (f (+ base y-weight) (rest ys))]))
+
+  (f 0 (sort xs
+             (lambda (x y)
+               (< (cdr x) (cdr y))))))
 
 
 (define (add-shadow el)
@@ -48,4 +82,13 @@
                  (string-append classes
                                 " "
                                 (first (shuffle css-font-sizes))))
+               ""))
+
+(define (add-font-face el)
+  (hash-update el
+               'class
+               (lambda (classes)
+                 (string-append classes
+                                " "
+                                (weighted-random-element css-font-faces)))
                ""))
